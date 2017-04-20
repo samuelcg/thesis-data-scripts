00c5e737985553e317ea0922faee74d65212b699
Sammy Gonzales-Luna <samuelcg@wic-medialab-rivers.princeton.edu>
2017-04-20 19:24:30 -0400

    Abandoning srank_func.

--- srank_func.R
+++ srank_func.R
@@ -28,6 +28,28 @@ srank_func <- function(list_of_dfs,
 	##################################
 	## Compute the spearmans_rank_coefficient across several fields for each pair of SAFE sessions
 	##################################
+	for (x in 1:nrow(session_pair_matrix)) {
+	  session_a <- session_pair_matrix[x,"SAFE_session_a"]
+	  session_b <- session_pair_matrix[x,"SAFE_session_b"]
+
+	  session_df_a <- list_of_npah_dfs[[session_a]]
+	  session_df_b <- list_of_npah_dfs[[session_b]]
+
+	  compute_full_row_of_src <- function(y) {
+	    attribute_str <- paste(toString(y),"_domain_num_enriched_attributes", sep="")
+
+	    src_N_domain_num_enriched_attributes <- compute_src(
+	      data_set_a=session_df_a[[attribute_str]],
+	      data_set_b=session_df_b[[attribute_str]])
+	    return(src_N_domain_num_enriched_attributes$estimate)
+	  }
+
+	  cleaned_names <- names(list_of_dfs[1])
+
+	  row_of_src <- sapply(num_non_src_cols:(num_non_src_cols + num_non_src_cols), compute_full_row_of_src)
+	  session_pair_matrix[x,(num_non_src_cols + 1):(ncol(session_pair_matrix))] <- row_of_src
+	  return(session_pair_matrix)
+	}
 
 
 }
