08ebf23cc3b0ada405eeee2f3385e81c1cd83b07
samuelcg <samuel.gonzalesluna@gmail.com>
2017-04-19 20:10:34 -0400

    Set up src result matrix for additional tests.

--- srank_npah.R
+++ srank_npah.R
@@ -9,19 +9,26 @@ session_pair_matrix <- t(combn(names(list_of_npah_dfs), 2))
 ##################################
 ## Add a column to the matrix to hold the results of the spearman's test
 ##################################
+num_src_tests_num_enriched_attributes <- 19
+
+src_matrix <- matrix(0, nrow=nrow(session_pair_matrix),ncol=num_src_tests_num_enriched_attributes + 1)
+
 session_pair_matrix <- cbind(
   session_pair_matrix,
-  1:nrow(session_pair_matrix),
-  1:nrow(session_pair_matrix))
+  src_matrix)
 
-colnames(session_pair_matrix) <- c(
+col_names <- 2:20
+col_names <- sapply(col_names, paste, "_src_domain_num_enriched_attributes", sep="")
+col_names <- c(c(
   "SAFE_session_a",
   "SAFE_session_b",
-  "src_neighborhood_score_predom",
-  "src_2_domain_num_enriched_attributes")
+  "src_neighborhood_score_predom"),
+  col_names)
+
+colnames(session_pair_matrix) <- col_names
 
 ##################################
-## Compute the spearmans_rank_coefficient for each pair of SAFE sessions
+## Compute the spearmans_rank_coefficient across several fields for each pair of SAFE sessions
 ##################################
 for (x in 1:nrow(session_pair_matrix)) {
   session_a <- session_pair_matrix[x,1]
@@ -30,13 +37,17 @@ for (x in 1:nrow(session_pair_matrix)) {
   session_df_a <- list_of_npah_dfs[[session_a]]
   session_df_b <- list_of_npah_dfs[[session_b]]
 
+  ##################################
+  ## neighborhood_score_predom src
+  ##################################
   src_neighborhood_score_predom <- compute_src(
     data_set_a=session_df_a$neighborhood_score_predom,
     data_set_b=session_df_b$neighborhood_score_predom)
   session_pair_matrix[x,"src_neighborhood_score_predom"] <- src_neighborhood_score_predom$estimate
 
+
   src_2_domain_num_enriched_attributes <- compute_src(
     data_set_a=session_df_a$'2_domain_num_enriched_attributes',
     data_set_b=session_df_b$'2_domain_num_enriched_attributes')
-  session_pair_matrix[x,"src_2_domain_num_enriched_attributes"] <- src_2_domain_num_enriched_attributes$estimate
+  session_pair_matrix[x,"2_src_domain_num_enriched_attributes"] <- src_2_domain_num_enriched_attributes$estimate
 }
