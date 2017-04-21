824a031d69f93874b335ef70df0077d362e1438b
Sammy Gonzales-Luna <samuelcg@wic-medialab-rivers.princeton.edu>
2017-04-20 21:02:01 -0400

    load_one_inverse_nsah

---
+++ load_one_inverse_nsah.R
@@ -0,0 +1,35 @@
+library(readr)
+
+ load_one_inverse_nsah <- function(arg_time_stamp) {
+   str_path_to_file <- paste("/Volumes/~samuelcg/thesis/Kamada-Kawai/raw-data/", arg_time_stamp,
+   "/neighborhood_scores_annotation-highest.txt", sep="")
+
+   nsah_t_df <- read_delim(str_path_to_file,
+     "\t", escape_double = FALSE, trim_ws = TRUE, skip = 5)
+   
+   nsah_t_df <- t(nsah_df)
+   nsah_t_df <- as.data.frame(nsah_t_df)
+   
+   helper <- sapply(nsah_t_df, is.factor)
+   nsah_t_df[helper] <- lapply(nsah_t_df[helper], as.character)
+   names(nsah_t_df) <- nsah_t_df[1,]
+
+
+
+
+   ##################################
+   ## Clean data - remove first row
+   ##################################
+   nsah_t_df <- nsah_t_df[-c(1),]
+
+   for(name in names(nsah_t_df)) {
+      if(name=="ORF") {
+         next
+      }
+      nsah_t_df[[name]]<- gsub("-0","0",nsah_t_df[[name]])
+
+      nsah_t_df[[name]]<- sapply(nsah_t_df[[name]], as.numeric)
+   }
+
+   return(nsah_t_df)
+ }
