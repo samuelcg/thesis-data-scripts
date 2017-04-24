eb5293d0569bd8dc6e3cd3be4f9323cfb7486590
Sammy Gonzales-Luna <samuelcg@wic-medialab-mason.princeton.edu>
2017-04-23 20:27:45 -0400

    More cleaning for NAs col of summary df.

--- clean_t_nsah_srank.R
+++ clean_t_nsah_srank.R
@@ -1,16 +1,26 @@
 clean_t_nsah_srank <- as.data.frame(t_nsah_session_pair_matrix)
-t_nsah_srank_summary <- matrix(0, ncol=6, nrow=3971)
+t_nsah_srank_summary <- matrix(0, ncol=7, nrow=3971)
 for(x in 3:ncol(clean_t_nsah_srank)) {
 	clean_t_nsah_srank[,x] <- sapply(clean_t_nsah_srank[,x], as.character)
 	clean_t_nsah_srank[,x] <- sapply(clean_t_nsah_srank[,x], as.numeric)
-	
-	one_summary <- unclass(summary(clean_t_nsah_srank[,x]))
-	t_nsah_srank_summary[x-2,] <- unclass(summary(t_nsah_srank_summary[,x]))
 
+	summary_length <- length(summary(clean_t_nsah_srank[,x]))
+	contains_nas <- summary_length > 6
+
+	if(contains_nas){
+		t_nsah_srank_summary[(x-2),] <- unclass(summary(clean_t_nsah_srank[,x]))
+	} else {
+		summary_row <- unclass(summary(clean_t_nsah_srank[,x]))
+		summary_row[7] <- 0
+		names(summary_row)[7] <- "NA\'s"
+		t_nsah_srank_summary[(x-2),] <- summary_row
+	}
+	
+	
 }
 
 t_nsah_srank_summary <- cbind(cols_to_compute, t_nsah_srank_summary)
-colnames(t_nsah_srank_summary) <- c("node_label", "min","q1","median","mean","q3","max")
+colnames(t_nsah_srank_summary) <- c("node_label", "min","q1","median","mean","q3","max", "NA\'s")
 t_nsah_srank_summary_df <- as.data.frame(t_nsah_srank_summary)
 
-rm(clean_t_nsah_srank, t_nsah_srank_summary,one_summary)
+rm(clean_t_nsah_srank, t_nsah_srank_summary)
