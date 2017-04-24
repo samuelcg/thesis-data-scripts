d2706c819529a1d85e6d715c922ebec7fdcd33eb
Sammy Gonzales-Luna <samuelcg@wic-medialab-mason.princeton.edu>
2017-04-24 16:52:51 -0400

    Config for fcdl analysis

--- t_nsah/dump_t_nsah_freq.R
+++ t_nsah/dump_t_nsah_freq.R
@@ -8,7 +8,7 @@ for(name in names(t_nsah_srank_summary_df)){
 	mytable <- table(t_nsah_srank_summary_df$node_label, t_nsah_srank_summary_df[[name]])
 	my_freq_table<-margin.table(mytable, 2)
 
-	layout_str <- "kk"
+	layout_str <- "fdcl"
 	file_name <- paste(layout_str,"t-nsah-go-terms",name,"srank.csv",sep="-")
 	write.csv(my_freq_table,file=file_name,row.names=TRUE)
 }
