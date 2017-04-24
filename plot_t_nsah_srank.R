d600087b8e747f70b6d1e179836cdb9dec6a2e47
Sammy Gonzales-Luna <samuelcg@wic-medialab-mason.princeton.edu>
2017-04-23 22:21:05 -0400

    Some plot formatting.

--- plot_t_nsah_srank.R
+++ plot_t_nsah_srank.R
@@ -1,1 +1,17 @@
-ggplot(t_nsah_srank_summary_df, aes(x=mean)) + geom_histogram() + geom_vline(xintercept = 0.8470)
+median_srank <- median(t_nsah_srank_summary_df$mean)
+str_median_srank <- as.character(median_srank)
+
+t_nsah_srank_plot <- ggplot(t_nsah_srank_summary_df, aes(x=mean)) 
+#histogram
+t_nsah_srank_plot <- t_nsah_srank_plot + geom_histogram() 
+# median line
+t_nsah_srank_plot <- t_nsah_srank_plot + geom_vline(xintercept = median_srank) 
+# remove grid
+t_nsah_srank_plot <- t_nsah_srank_plot + theme(panel.grid.major = element_blank(),
+	panel.grid.minor=element_blank())
+# Line label
+t_nsah_srank_plot <- t_nsah_srank_plot + annotate("text", 
+	x=median_srank, 
+	y=1000, 
+	label=paste("Median~rho==",str_median_srank),
+	parse=TRUE)
